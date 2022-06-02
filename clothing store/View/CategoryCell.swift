ba3689b440bdba124e2c2b954df0b687c053deba
TearsOprah <qmeshokp@mail.ru>
2022-06-02 21:09:37 +0300

    cell prototype

---
+++ clothing store/View/CategoryCell.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryCell.swift
+//  clothing store
+//
+//  Created by meshok on 02.06.2022.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
